fa7e63c2ff9de2ea1796a4e3b8c1f13ab09a5344
JoseDeep <josephdeepan06@gmail.com>
2025-02-07 20:53:20 +0530

    initial

--- contactbook.py
+++ contactbook.py
@@ -1,43 +1,62 @@
-import json
+class Contact:
+    def __init__(self, name, phone, email):
+        self.name = name
+        self.phone = phone
+        self.email = email
 
-class ContactManager:
-    def __init__(self, filename='contacts.json'):
-        self.filename = filename
-        self.contacts = self.load_contacts()
+    def __str__(self):
+        return f"Name: {self.name}, Phone: {self.phone}, Email: {self.email}"
 
-    def load_contacts(self):
-        try:
-            with open(self.filename, 'r') as file:
-                return json.load(file)
-        except FileNotFoundError:
-            return []
+class ContactBook:
+    def __init__(self):
+        self.contacts = []
 
-    def save_contacts(self):
-        with open(self.filename, 'w') as file:
-            json.dump(self.contacts, file, indent=4)
-
-    def add_contact(self, name, phone, email, address):
-        contact = {
-            'name': name,
-            'phone': phone,
-            'email': email,
-            'address': address
-        }
+    def add_contact(self, contact):
         self.contacts.append(contact)
-        self.save_contacts()
 
-    def view_contacts(self):
+    def remove_contact(self, name):
+        self.contacts = [contact for contact in self.contacts if contact.name != name]
+
+    def find_contact(self, name):
         for contact in self.contacts:
-            print(f"Name: {contact['name']}, Phone: {contact['phone']}")
+            if contact.name == name:
+                return contact
+        return None
 
-    def search_contact(self, search_term):
-        results = [contact for contact in self.contacts if search_term in contact['name'] or search_term in contact['phone']]
-        for contact in results:
-            print(f"Name: {contact['name']}, Phone: {contact['phone']}, Email: {contact['email']}, Address: {contact['address']}")
+    def list_contacts(self):
+        return self.contacts
 
-    def update_contact(self, name, new_details):
-        for contact in self.contacts:
-            if contact['name'] == name:
-                contact.update(new_details)
-                self.save_contacts()
-                return
+if __name__ == "__main__":
+    book = ContactBook()
+    while True:
+        print("\nContact Book Menu:")
+        print("1. Add Contact")
+        print("2. Remove Contact")
+        print("3. Find Contact")
+        print("4. List Contacts")
+        print("5. Exit")
+        choice = input("Choose an option: ")
+
+        if choice == '1':
+            name = input("Enter name: ")
+            phone = input("Enter phone: ")
+            email = input("Enter email: ")
+            book.add_contact(Contact(name, phone, email))
+        elif choice == '2':
+            name = input("Enter name of the contact to remove: ")
+            book.remove_contact(name)
+        elif choice == '3':
+            name = input("Enter name of the contact to find: ")
+            contact = book.find_contact(name)
+            if contact:
+                print(contact)
+            else:
+                print("Contact not found.")
+        elif choice == '4':
+            contacts = book.list_contacts()
+            for contact in contacts:
+                print(contact)
+        elif choice == '5':
+            break
+        else:
+            print("Invalid choice. Please try again.")
